24f4237b3681655207b066b391f7cb502d5067bd
Thonedra <thonedra.dev@gmail.com>
2026-01-11 18:56:21 +0800

    Defining the basic states, that will be needed in my program

--- TaskManager/state.js
+++ TaskManager/state.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const state = {
+    tasks: [],                   // all Tasks here
+    status_filter:   'all',      // filtering between ==> [all, pending, complete]
+    priority_filter: 'all',      // filtering between ==> [all, low, medium, high]
+    search_query:       '',      // empty string still.
+    editing_id:       null,      // null still
+     
+
+}
