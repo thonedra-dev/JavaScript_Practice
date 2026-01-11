45e18c3c88609b2968b452182e1af6e73c447399
Thonedra <thonedra.dev@gmail.com>
2026-01-11 19:25:23 +0800

    Adding mini-comment documentation

--- TaskManager/taskService.js
+++ TaskManager/taskService.js
@@ -1,16 +1,18 @@
 const taskService =
 {
     addTask: function(title, priority)
-    {
-        if (title.trim() === '') { return false; }
-        const new_task = this.createTask(title, priority);
-        state.tasks.push(new_task);
-        state.save();
-    },
+    {                                                                //In here, we have to understand that, 
+        if (title.trim() === '') { return false; }                   //We could just call the state.save(new_task),
+        const new_task = this.createTask(title, priority);           //But state.tasks, that state will be empty even,
+        state.tasks.push(new_task);                                  //a new task entered the localStoage, because, state()
+        state.save();                                                //only calls the storage.init() to save it but nothing is
+    },                                                               //updating the state.tasks, which will be used later for many references
 
-    createTask: function(title, priority) 
-    {
-        return {
+
+
+    createTask: function(title, priority)                            //This is just helper function to fill,
+    {                                                                //necessary details of a single task record.
+        return {       
             id: utils.generateId(),
             title: title.trim(),
             priority: priority || 'medium',
