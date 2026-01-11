bec53d129384e319cecbfb7999463a845bb80e7c
Thonedra <thonedra.dev@gmail.com>
2026-01-11 19:47:32 +0800

    ..

--- TaskManager/taskService.js
+++ TaskManager/taskService.js
@@ -5,7 +5,7 @@ const taskService =
         if (title.trim() === '') { return false; }                   //We could just call the state.save(new_task),
         const new_task = this.createTask(title, priority);           //But state.tasks, that state will be empty even,
         state.tasks.push(new_task);                                  //a new task entered the localStoage, because, state()
-        state.save();                                                //only calls the storage.init() to save it but nothing is
+        state.save();  return true;                                  //only calls the storage.init() to save it but nothing is
     },                                                               //updating the state.tasks, which will be used later for many references
 
 
